319014b57e70c7457d1cccba68f8b0f1096fb5b0
Simon Maloneay <simonmaloneay@gmail.com>
2023-07-11 20:13:20 -0300

    add: modelo Estudiante, profesor, curso

--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    direccion = models.CharField(max_length=200)
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=100)
+    especialidad = models.CharField(max_length=100)
+    salario = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+    duracion = models.IntegerField()
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    estudiantes = models.ManyToManyField(Estudiante)
