4f6e67d03cdc02d0a2b696c18097ef81685ace69
Simon Maloneay <simonmaloneay@gmail.com>
2023-07-12 20:33:29 -0300

    productos urls view indezx

--- project/apps/Cliente/views.py
+++ project/apps/Cliente/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+
 from .models import Cliente
+
 def home(request):
     Clientes_registros= Cliente.objects.all()
     return render(request,"index.html", {"Clientes", Clientes_registros})
--- project/apps/Producto/views.py
+++ project/apps/Producto/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    contexto={"app": "Aplicacion Producto "}
+    return render(request,"index.html", {"Clientes", Clientes_registros})
