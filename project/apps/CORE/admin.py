661e778a870bb7bc42a3c4172023d3d97098e5e7
Simon Maloneay <simonmaloneay@gmail.com>
2023-07-12 20:18:21 -0300

    borrar core

--- project/apps/CORE/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from . import models
-
-admin.site.register(models.Curso)
-admin.site.register(models.Estudiante)
-admin.site.register(models.Profesor)
