f5f3dbc3e8bf831b20aa94f00e58f14b1e7e73ec
Simon Maloneay <simonmaloneay@gmail.com>
2023-07-12 20:21:51 -0300

    ASNMIFJSADFNJIASDBHF

--- project/apps/Cliente/views.py
+++ project/apps/Cliente/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from .models import Cliente
 def home(request):
     Clientes_registros= Cliente.objects.all()
-    return render(request,"index.html", {"clientes", Clientes_registros})
+    return render(request,"index.html", {"Clientes", Clientes_registros})
