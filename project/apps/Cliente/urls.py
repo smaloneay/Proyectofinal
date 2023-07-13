8e5aaae214107306e515613da4162849d313ef9f
Simon Maloneay <simonmaloneay@gmail.com>
2023-07-13 01:14:53 -0300

    clientes

--- project/apps/Cliente/urls.py
+++ project/apps/Cliente/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, crear_clientes
 app_name="Cliente"
 urlpatterns = [
     path('', home, name="home"),
+    path('crear_clientes', crear_clientes, name="crear_clientes")
 ]
--- project/apps/Cliente/views.py
+++ project/apps/Cliente/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Cliente
+from .models import Cliente, Pais
 
 def home(request):
     Clientes_registros= Cliente.objects.all()
-    contexto={"clientes": Clientes_registros}
+    contexto={"Cliente": Clientes_registros}
     return render(request, "Cliente/index.html", contexto)
     #return render(request,"index.html", {"Cliente", Clientes_registros})
+
+def crear_clientes(request):
+    from datetime import date
+
+    p1= Pais(nombre="Mexico")
+    p2= Pais(nombre= "Peru")
+    p3= Pais(nombre= "Uruguay")
+
+    p1.save()
+    p2.save()
+    p3.save()
+
+    c1= Cliente(nombre="Almendra", apellido="Darwin", nacimiento=date(2015,1,1),pais_origen_id=p1)
+    c2= Cliente(nombre="Pepe", apellido="Jose", nacimiento=date(2000,5,8),pais_origen_id=p2)
+    c3= Cliente(nombre="Juan", apellido="Sanchez", nacimiento=date(2002,2,3),pais_origen_id=p3)
+    c4= Cliente(nombre="Sofia", apellido="Andrio", nacimiento=date(2004,4,5),pais_origen_id=None)
+
+    c1.save()
+    c2.save()
+    c3.save()
+    c4.save()
+    return redirect("Cliente:home")
